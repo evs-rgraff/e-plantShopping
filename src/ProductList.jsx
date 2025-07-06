299f4f4473462bf1f159fa7edb8b27048a3def6a
Your Name <you@example.com>
2025-07-06 12:02:45 -0400

    add description and price

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -290,6 +290,12 @@ function ProductList({ onHomeClick }) {
                                         <div className="product-title">
                                             {plant.name}
                                         </div>
+                                        <div className="product-description">
+                                            {plant.description}
+                                        </div>
+                                        <div className="product-cost">
+                                            ${plant.cost}
+                                        </div>
                                     </div>
                                 ))}
                             </div>
